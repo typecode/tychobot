7fa3310e21c9a46389e563f6912cf3cf4b0e0009
Matt Briancon <matt.briancon@gmail.com>
2013-08-07 14:16:46 -0400

    Less spewing, more eating

--- tychobot.py
+++ tychobot.py
@@ -9,13 +9,21 @@ from twisted.words.protocols import irc
 def parse_nickname(user):
     return user.split('!')[0].lstrip('@')
 
+RESTAURANTS = {
+    'tenda': ('Tenda Asian Bistro', 'http://www.seamless.com/food-delivery/tenda-asian-bistro-brooklyn.7457.r#menu-tab'),
+    'tutt': ('Tutt Cafe', 'http://www.tuttcafe.com/menu.html'),
+    'indian': ('Curry Heights', 'http://www.seamless.com/food-delivery/curry-heights-brooklyn.7023.r#menu-tab'),
+}
+
+RESTAURANT_RE = re.compile(r'({})'.format('|'.join(RESTAURANTS)))
+
 
 class TychoBot(irc.IRCClient):
-    nickname = 'matt'
-    username = 'matt'
-    password = 'mattmattmatt'
+    nickname = 'tycho'
+    username = 'tycho'
+    password = 'tycho'
 
-    channels = ['#bot-testing']
+    channels = ['#lobby', '#architizer', '#bot-testing']
 
     admins = ['matt']
 
@@ -35,10 +43,11 @@ class TychoBot(irc.IRCClient):
             self.join(channel)
 
     def joined(self, channel):
-        print('joined', channel)
+        # print('joined', channel)
+        pass
 
     def privmsg(self, user, channel, message):
-        print(user, channel, message)
+        # print(user, channel, message)
         user = parse_nickname(user)
 
         if channel == self.nickname:
@@ -52,22 +61,28 @@ class TychoBot(irc.IRCClient):
         self.handle_general(user, channel, message)
 
     def handle_whisper(self, user, message):
-        print('handle_whisper')
+        # print('handle_whisper')
         self.msg(user, 'I like to whisper too!')
 
     def handle_command(self, user, channel, message):
         print('handle_command')
 
     def handle_general(self, users, channel, message):
-        print('handle_general')
+        # print('handle_general')
         hay = r'(hey |hay )?(guys|guise)'
         if re.match(hay, message):
             self.ping_channel(channel)
+
+        for name in RESTAURANT_RE.findall(message.lower()):
+            self.say(channel, '{}: {}'.format(*RESTAURANTS[name]))
 
     def ping_channel(self, channel):
         print('ping_channel')
 
         def got_names(name_list):
+            name_list = [parse_nickname(nick) for nick in name_list]
+            name_list.remove(self.nickname)
+
             self.say(channel, ' '.join(name_list))
 
         self.names(channel).addCallback(got_names)
@@ -115,7 +130,7 @@ class TychoBotFactory(protocol.ClientFactory):
 
     def clientConnectionLost(self, connector, reason):
         print('Connection Lost', reason)
-        connector.connect()
+        # connector.connect()
 
     def clientConnectionFailed(self, connector, reason):
         print('Connection Failed', reason)
